06076cfafd398895eccfc2cb1b98061e7a4df0b9
avdhoot <avdhootlad4782@gmail.com>
2026-03-11 18:12:43 +0530

    commit

--- main.tf
+++ main.tf
@@ -152,6 +152,8 @@ resource "aws_instance" "jume" {
     sed -i '$i <Resource name="jdbc/TestDB" auth="Container" type="javax.sql.DataSource" maxTotal="500" maxIdle="30" maxWaitMillis="1000" username="arya" password="Aryakadam47" driverClassName="com.mysql.jdbc.Driver" url="jdbc:mysql://${aws_db_instance.mydb.endpoint}:3306/studentapp?useUnicode=yes&characterEncoding=utf8"/>' $FILE
     /opt/apache-tomcat-9.0.115/bin/./catalina.sh stop
     /opt/apache-tomcat-9.0.115/bin/./catalina.sh start
+
+    
     yum install mariadb105* -y
     systemctl start mariadb.service
     systemctl enable mariadb.service
