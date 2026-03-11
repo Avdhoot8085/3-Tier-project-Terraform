3f663bec13b5055b21599ad7653002c8a09f7ff8
avdhoot <avdhootlad4782@gmail.com>
2026-03-11 16:54:42 +0530

    commit

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ provider "aws" {
 # create a VPC.
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   
   tags = {
     Name = "my-vpc"
