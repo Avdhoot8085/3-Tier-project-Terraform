a2c03691e99917ecbd0547ccab709e25b1233f4a
avdhoot <avdhootlad4782@gmail.com>
2026-03-12 12:54:29 +0530

    commit

--- main.tf
+++ main.tf
@@ -206,6 +206,7 @@ resource "aws_db_instance" "mydb" {
   password             = "admin123"
   db_subnet_group_name = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  skip_final_snapshot = true
 }
 
 
